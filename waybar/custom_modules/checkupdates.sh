0c562ae356b37a127788af45f6bfb88d479f7911
un3481 <78743473+un3481@users.noreply.github.com>
2023-10-29 11:09:06 -0300

    improve checkupdates waybar module

--- waybar/custom_modules/checkupdates.sh
+++ waybar/custom_modules/checkupdates.sh
@@ -84,6 +84,6 @@ waybar_json() {
 # main
 outdated=$(check_outdated $LASTRUN_FILE 300)
 if [[ "$outdated" == "outdated" ]]; then
-  (nohup (checkupdates --nocolor)) &
+  (nohup checkupdates) &
 fi
 waybar_json
